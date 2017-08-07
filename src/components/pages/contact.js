1ca718f11a746f02469d8df0382ee2370677e1c2
Begüm Eryıldız <begumeryildiz@gmail.com>
2017-08-07 14:47:40 +0200

    first commit.

---
+++ src/components/pages/contact.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class Contact extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+        <h1>
+          Contact
+        </h1>
+
+        <p>
+         Contact us
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/components/pages/products.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class Products extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+
+        <h1>
+          Products
+        </h1>
+
+        <p>
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
+        </p>
+
+      </div>
+    );
+  }
+}
+
+export default Products;
